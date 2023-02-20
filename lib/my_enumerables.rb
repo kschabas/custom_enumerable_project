c4121bf62e9dd2a416cb5d71d5d448b0a146723c
Karl Schabas <karl.schabas@utoronto.ca>
2023-02-20 09:33:09 -0500

    Complete first three exercises

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,5 +1,19 @@
 module Enumerable
   # Your code goes here
+  def my_each_with_index
+    for i in 0...self.length do
+      yield(self[i], i)
+    end
+    self
+  end
+
+  def my_select
+    result_array = []
+    for i in self do
+      result_array << i if yield(i)
+    end
+    result_array
+  end
 end
 
 # You will first have to define my_each
@@ -8,4 +22,10 @@ end
 # to this method
 class Array
   # Define my_each here
+  def my_each
+    for i in self do
+      yield(i)
+    end
+    self
+  end
 end
